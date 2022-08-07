a549ff2942e74acce1f1cfb9051bc7ef8c2db497
Diogo Araújo <61624282+Diogo-a21905661@users.noreply.github.com>
2022-08-07 02:09:25 +0100

    Uploaded new submission

---
+++ src/org/dropProject/samples/sampleKotlinSubmission/Main.kt
@@ -0,0 +1,18 @@
+package org.dropProject.samples.sampleKotlinSubmission
+
+fun findMax(numbers: Array<Int>) : Int {
+    var max = numbers[0]
+    for (number in numbers) {
+        if (max < number) {
+            max = number
+        }
+    }
+
+    return max
+}
+
+fun main() {
+    val numbers = arrayOf(1, 2, 3, -6, -7)
+
+    println(findMax(numbers))
+}
